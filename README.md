daa260ed4bb81da1e40f515fe3cbe2af1f14e5ec
edbertjk <qeuans12@gmail.com>
2024-08-03 10:39:33 +0700

    Added Boost Asio Into Project

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 ## Learn Async I/O In Boost.asio
 Playlist: https://youtube.com/playlist?list=PLIXt8mu2KcUJOwdLMp-Z-cDIZA1aZfVTN&si=unaE3OKu3iTsy0Rq
+
+
+## Installation
+1. Install Boost.asio use sudo apt-get install libboost-all-dev
+2. Add Boost.asio into c_cpp_project "includePath": [ "/usr/include/boost" ],
+3. Import Boost.asio into your project #include <boost/asio.hpp> & using namespace boost;
+4. Compile and run the project (g++ -o ./build/a.out main.cpp -I/usr/include/boost -lboost_system)
--- main.cpp
+++ main.cpp
@@ -1,7 +1,33 @@
 #include <bits/stdc++.h>
-using namespace std;
+#ifdef _WIN32
+#define _WIN32_WINNT 0x0A00
+#endif
 
-int main(){
-    cout <<  "still learning about socket, thread, etc in boost asio" << endl;
+#define ASIO_STANDALONE
+#include <boost/asio.hpp>
+#include <asio/ts/buffer.hpp>
+#include <asio/ts/internet.hpp>
+using namespace std;
+using namespace boost;
+
+int main() {
+    asio::io_context io_context; 
+
+    system::error_code ec;
+
+    asio::ip::tcp::endpoint endPoint(asio::ip::address::from_string("109.110.188.7", ec), 80);
+
+    asio::ip::tcp::socket socket(io_context);
+
+    socket.connect(endPoint, ec);
+
+    if (ec) {
+        std::cout << "Error code: " << ec.value() << " Message: " << ec.message() << std::endl;
+    }
+    else {
+        std::cout << "Connected!" << std::endl;
+    }
+
+    std::system("pause");
     return 0;
 }
